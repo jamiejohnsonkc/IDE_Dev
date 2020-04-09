211ac501da3c821a194871c03f8ed8b6c1557859
jamiejohnsonkc <jamie@jamie-johnson.com>
2020-04-09 11:51:50 -0500

    removed MDX pages dir from components dir, removed content as a gatsby-source-filesystem from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/content/`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-react-svg`,
       options: {},
     },
@@ -76,7 +69,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
